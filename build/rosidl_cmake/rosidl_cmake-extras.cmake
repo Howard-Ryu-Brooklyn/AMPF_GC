071cdc8d30bebddbe77d6fe12b72147f86e8eb1d
humble <fbdlscks12@gmail.com>
2024-01-02 15:23:21 +0900

    final

--- build/rosidl_cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(my_interfaces_IDL_FILES "msg/BoundingBox.idl;msg/PolarCoor.idl;msg/Uwb.idl;msg/DesiredFormation.idl;msg/Follower1Info.idl;msg/Follower2Info.idl;msg/Localization.idl")
-set(my_interfaces_INTERFACE_FILES "msg/BoundingBox.msg;msg/PolarCoor.msg;msg/Uwb.msg;msg/DesiredFormation.msg;msg/Follower1Info.msg;msg/Follower2Info.msg;msg/Localization.msg")
